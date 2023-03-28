49acb938308dcf6b1f4cfe28ff578abe04b1ffb1
谁书-ss <yushuishu@qq.com>
2023-03-28 08:33:12 +0800

    feat:Elasticsearch

---
+++ src/main/java/com/shuishu/demo/elasticsearch/ElasticsearchExamplesApplication.java
@@ -0,0 +1,22 @@
+package com.shuishu.demo.elasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author ：谁书-ss
+ * @date   ： 2023-03-28 8:12
+ * @IDE    ：IntelliJ IDEA
+ * @Motto  ：ABC(Always Be Coding)
+ * <p></p>
+ * @Description ：Elasticsearch 学习
+ */
+@SpringBootApplication
+public class ElasticsearchExamplesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElasticsearchExamplesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shuishu/demo/elasticsearch/ElasticsearchExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shuishu.demo.elasticsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticsearchExamplesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
